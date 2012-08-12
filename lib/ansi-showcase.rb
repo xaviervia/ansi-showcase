a82ba38cef61c92cce692ff681bc80c0e718a230
xavier <xavier@xavier-pc.(none)>
2012-08-12 05:45:26 -0300

    Development seems finished, now lets document, test and publish

---
+++ lib/ansi-showcase.rb
@@ -0,0 +1,7 @@
+require "ansi-showcase/version"
+
+module Ansi
+  module Showcase
+    # Your code goes here...
+  end
+end
---
+++ lib/ansi-showcase/version.rb
@@ -0,0 +1,5 @@
+module Ansi
+  module Showcase
+    VERSION = "0.0.1"
+  end
+end
